4dfc69caf138084fea8d3d4db5ff4f2e4ff64f6b
swatinagare4 <n.swati111@gmail.com>
2023-01-30 19:30:29 +0530

    changes by swati

---
+++ src/main/java/com/Swati/GitUseMavenProject2Application.java
@@ -0,0 +1,13 @@
+package com.Swati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitUseMavenProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitUseMavenProject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Swati/GitUseMavenProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Swati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitUseMavenProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
